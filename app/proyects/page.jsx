fd025b543c1811d59617971e95a8bf1aa33a3e50
ni7co7 <nicolasgianfelici84@gmail.com>
2026-03-29 19:43:16 -0300

    nuevos proyects logo chico

--- app/proyects/page.jsx
+++ app/proyects/page.jsx
@@ -154,7 +154,62 @@ export default function Proyects() {
                   Implementes radio buttons for select type of material and text inputs for dimension. Calculates prices automatically. Data base manage information of clients, products stock, pay methods and orders.
               </p>
               <a
-                href="https://moviledoctor.netlify.app/"
+                href="https://www.linkedin.com/posts/nicogian_ugcPost-7443802344120610816-EXau?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC54zTsBruuUSFXZJ7rPPqQOFdTZAQsXskU"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-2 border-violet-500 bg-white text-violet-500 px-4 py-2 rounded-lg font-semibold hover:bg-violet-500 hover:text-white transition duration-300 mt-auto"
+              >
+                See
+              </a>
+            </div>
+          </div>
+
+
+
+           {/* Card 6 */}
+          <div className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition duration-300">
+            <div className="w-full h-150 overflow-hidden">
+                                                                                    
+             <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7444121162080665600?collapsed=1" 
+             height="442" 
+             width="504" 
+             frameborder="0" 
+             allowfullscreen="" 
+             title="Publicación integrada">
+
+             </iframe>        
+             
+            </div>
+            <div className="p-6 flex flex-col flex-grow">
+              <h3 className="text-slate-500 text-5xl font-semibold mb-2">Apuntador de Acordes</h3>
+              <p className="text-2xl text-gray-600 mb-4">
+                  App for practicing piano chords. Develop in Android Studio. You can record any chord and save those armonics sequences you are looking for. 
+              </p>
+              <a
+                href="https://www.linkedin.com/posts/nicogian_activity-7444121194230046720-CIGT?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC54zTsBruuUSFXZJ7rPPqQOFdTZAQsXskU"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-2 border-violet-500 bg-white text-violet-500 px-4 py-2 rounded-lg font-semibold hover:bg-violet-500 hover:text-white transition duration-300 mt-auto"
+              >
+                See
+              </a>
+            </div>
+          </div>
+         
+           {/* Card 7 */}
+          <div className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-2xl transition duration-300">
+            <div className="w-full h-150 overflow-hidden">
+                                                                                    
+             
+             <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7444147612879974400?collapsed=1" height="442" width="504" frameborder="0" allowfullscreen="" title="Publicación integrada"></iframe>
+            </div>
+            <div className="p-6 flex flex-col flex-grow">
+              <h3 className="text-slate-500 text-5xl font-semibold mb-2">IAT</h3>
+              <p className="text-2xl text-gray-600 mb-4">
+                  Desktop App. Implicit Association Test. Develop whith pure Js. You can create your own test with your own categories and stimuli. It calculates the average response time for each category and shows the results in a table.  
+              </p>
+              <a
+                href="https://steady-lolly-4491c1.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="border-2 border-violet-500 bg-white text-violet-500 px-4 py-2 rounded-lg font-semibold hover:bg-violet-500 hover:text-white transition duration-300 mt-auto"
